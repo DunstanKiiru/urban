c6a6e64a04da3d3eb05d4ec4eef4c25a3afeb0e5
Alex Kiiru <alexkiiru7@gmail.com>
2026-09-05 20:58:22 +0300

    updates about page

---
+++ resources/views/about.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('title', 'About Us | Urban Roads SACCO')
+
+@section('content')
+
+    <section class="mx-auto max-w-7x1 px-6 py-20">
+        <h1 class="text-4x1 font-bold">
+            About Urban Roads SACCO
+        </h1>
+
+        <p class="mt-4 text-gray-600">
+            Learn more about Urban Roads SACCO, our mission, vision and commitment to our members.
+        </p>
+    </section>
+
+    @endsection
