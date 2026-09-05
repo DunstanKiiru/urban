cb9f370ed2e4e4435db320c22af8aabb9246116e
Alex Kiiru <alexkiiru7@gmail.com>
2026-09-05 20:52:22 +0300

    updtaes homepage

---
+++ resources/views/home.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('title', 'Home | Urban Roads SACCO')
+
+@section('content')
+
+    <section class="mx-auto max-w-7xl px-6 py-20">
+        <h1 class="text-4x1 font-bold">
+            Welcome to Urban Roads SACCO
+        </h1>
+
+        <p class="mt-4 text-lg text-gray-600">
+            Your trusted partner for savings, loans and financial growth.
+        </p>
+    </section>
+
+     @endsection
