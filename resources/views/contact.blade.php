4702d72c8ae6237f2a455f33dccc960bd7b6d15b
Alex Kiiru <alexkiiru7@gmail.com>
2026-09-05 20:58:50 +0300

    updates contact page

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('title', 'Contact Us | Urban Roads SACCO')
+
+@section('content')
+
+    <section class="mx-auto max-w-7xl px-6 py-20">
+        <h1 class="text-4xl font-bold">
+            Contact Us
+        </h1>
+
+        <p class="mt-4 text-gray-600">
+            Get in touch with Urban Roads SACCO.
+        </p>
+    </section>
+
+@endsection
