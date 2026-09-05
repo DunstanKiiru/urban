b78e4f86bb31fb31693a26802e6d31c0503d8cb2
Alex Kiiru <alexkiiru7@gmail.com>
2026-09-05 20:18:26 +0300

    Updates main layout in app blade file

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,23 @@
-<x-layouts::app.sidebar :title="$title ?? null">
-    <flux:main>
-        {{ $slot }}
-    </flux:main>
-</x-layouts::app.sidebar>
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="Viewport" content="width=device-width, initial-scale=1.0">
+
+        <title>
+            @yield('title','Urban Roads SACCO')
+        </title>
+
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    </head>
+    <body class="bg-white text-gray-900 antialised">
+        @include('components.navbar')
+
+        <main>
+            @yield('content')
+        </main>
+
+        @include('components.footer')
+    </body>
+
+</html>
