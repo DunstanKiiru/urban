e8d22b3b8914053b0f78903bbdbb22ff5da8b2ce
Alex Kiiru <alexkiiru7@gmail.com>
2026-09-05 19:44:38 +0300

    updates web routes

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ContactController;
 
-Route::view('/', 'welcome')->name('home');
+Route::get('/', [HomeController::class, 'index'])->name('home');
+
+
+Route::get('/about', [AboutController::class, 'index'])->name('about');
+
+
+Route::get('/contact', [ContactController::class, 'index'])->name('contact');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::view('dashboard', 'dashboard')->name('dashboard');
