94ae445cd5d87ade1290d1fe6bc97694f6042869
Alex Kiiru <alexkiiru7@gmail.com>
2026-09-05 20:29:40 +0300

    Updates navbar

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,23 @@
+<header class ="border-b border-gray-200 bg-white">
+    <nav class="mx-auto flex max-w 7xl items-center justify-between px-6 py-4">
+        <a href="{{ route('home') }} class="text-x1 font-bold">
+            Urban Roads SACCO
+        </a>
+
+<div class="hidden items-center gap-8 md:flex">
+    <a href="{{ route('home') }}">Home</a>
+    <a href="{{ route('about') }}">About Us</a>
+    <a href="#">Membership</a>
+    <a href="#">Savings</a>
+    <a href="#">Loans</a>
+    <a href="#">Services</a>
+    <a href="{{ route('contact') }}">Contact</a>
+</div>
+<a
+            href="#"
+            class="rounded-lg bg-blue-700 px-5 py-2.5 font-semibold text-white"
+        >
+            Member Login
+        </a>
+    </nav>
+</header>
