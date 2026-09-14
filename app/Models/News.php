ce79241643af006d2609f6098069360a7275510b
Alex Kiiru <alexkiiru7@gmail.com>
2026-09-14 13:49:23 +0300

    updates news model

--- app/Models/News.php
+++ app/Models/News.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class News extends Model
 {
-    //
+    protected $fillable = [
+        'title',
+        'slug',
+        'excerpt',
+        'content',
+        'image',
+        'published_at',
+        'is_published',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'published_at' => 'datetime',
+            'is_published' => 'boolean',
+        ];
+    }
 }
